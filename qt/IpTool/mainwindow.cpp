5d48617bb38f75abaff8f93e99dbb922fd363ead
wanghw <wanghaowen@cecgw.cn>
2019-03-06 10:23:37 +0800

    add(IpTool): 增加IP地址转换工具

---
+++ qt/IpTool/mainwindow.cpp
@@ -0,0 +1,38 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+#include <QHostAddress>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    connect(ui->lineEdit_ip, &QLineEdit::textEdited, [this] {
+        QString str = ui->lineEdit_ip->text();
+        QHostAddress addr(str);
+        if (addr.isNull()) {
+            ui->lineEdit_ip->setStyleSheet("color: red");
+        } else {
+            ui->lineEdit_ip->setStyleSheet("color: black");
+            ui->doubleSpinBox_int->setValue(addr.toIPv4Address());
+        }
+    });
+
+    connect(ui->doubleSpinBox_int, &QDoubleSpinBox::editingFinished, [this] {
+        quint32 val = (quint32)ui->doubleSpinBox_int->value();
+        QHostAddress addr(val);
+        if (addr.isNull()) {
+            ui->doubleSpinBox_int->setStyleSheet("color: red");
+        } else {
+            ui->doubleSpinBox_int->setStyleSheet("color: black");
+            ui->lineEdit_ip->setText(addr.toString());
+        }
+    });
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
