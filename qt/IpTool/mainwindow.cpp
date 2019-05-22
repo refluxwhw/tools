8970c17de78948323df0eaec29a10b04c3745785
wanghw <wanghaowen@cecgw.cn>
2019-05-22 17:45:39 +0800

    fix: 字节序错误问题

--- qt/IpTool/mainwindow.cpp
+++ qt/IpTool/mainwindow.cpp
@@ -2,12 +2,14 @@
 #include "ui_mainwindow.h"
 #include <QDebug>
 #include <QHostAddress>
+#include <QtEndian>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setWindowFlags(windowFlags() & ~Qt::WindowMinMaxButtonsHint);
 
     connect(ui->lineEdit_ip, &QLineEdit::textEdited, [this] {
         QString str = ui->lineEdit_ip->text();
@@ -16,19 +18,28 @@ MainWindow::MainWindow(QWidget *parent) :
             ui->lineEdit_ip->setStyleSheet("color: red");
         } else {
             ui->lineEdit_ip->setStyleSheet("color: black");
-            ui->doubleSpinBox_int->setValue(addr.toIPv4Address());
+            quint32 val = addr.toIPv4Address(); // 主机字节序
+            ui->doubleSpinBox_host->setValue(val);
+            ui->doubleSpinBox_network->setValue(qToBigEndian(val));
         }
     });
 
-    connect(ui->doubleSpinBox_int, &QDoubleSpinBox::editingFinished, [this] {
-        quint32 val = (quint32)ui->doubleSpinBox_int->value();
+    connect(ui->doubleSpinBox_network, &QDoubleSpinBox::editingFinished, [this] {
+        quint32 val = (quint32)ui->doubleSpinBox_network->value();
+        val = qToBigEndian(val);
+        ui->doubleSpinBox_host->setValue(val);
+
+        QHostAddress addr(val); // 要求传入主机字节序
+        ui->lineEdit_ip->setText(addr.toString());
+    });
+
+    connect(ui->doubleSpinBox_host, &QDoubleSpinBox::editingFinished, [this] {
+        quint32 val = (quint32)ui->doubleSpinBox_host->value();
         QHostAddress addr(val);
-        if (addr.isNull()) {
-            ui->doubleSpinBox_int->setStyleSheet("color: red");
-        } else {
-            ui->doubleSpinBox_int->setStyleSheet("color: black");
-            ui->lineEdit_ip->setText(addr.toString());
-        }
+        ui->lineEdit_ip->setText(addr.toString());
+
+        val = qToBigEndian(val); // 转为网络字节序
+        ui->doubleSpinBox_network->setValue(val);
     });
 }
 
